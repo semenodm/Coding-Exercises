8ad207f6822401d34087f3100c7d1fbffd0228db
dsemenov <sdo.semenov@gmail.com>
2012-11-29 01:04:03 +0200

    Heap Data structure implementation

--- org.sdo.alg/src/main/java/org/sdo/algorythms/heap/Heap.java
+++ org.sdo.alg/src/main/java/org/sdo/algorythms/heap/Heap.java
@@ -6,25 +6,144 @@ import java.util.List;
 
 public class Heap {
 	private List<Integer> storage;
+
 	public Heap(Integer[] integers) {
-		this.storage = new ArrayList<Integer>(Arrays.asList(integers));
+		this.storage = new ArrayList<>(Arrays.asList(integers));
 	}
-	public Integer parent(int i) {		
-		return storage.get((i-1)/2);
+
+	public Heap() {
+		this.storage = new ArrayList<>();
 	}
-	public Integer[] children(int i) {		
-		return new Integer[]{storage.get((i+1)*2 -1 ),storage.get((i+1)*2)};
+
+	private int parentIndex(int i) {
+		return (i + 1) / 2 - 1;
 	}
+
+	private int firstChildIndex(int i) {
+		return secondChildIndex(i) - 1;
+	}
+
+	private int secondChildIndex(int i) {
+		return (i + 1) * 2;
+	}
+
 	public Integer extract() {
-		return storage.remove(0);
+		Integer root = rootElement();
+		if (heapHasMoreThenOneElement()) {
+			swap(0, lastIndex());
+			storage.remove(lastIndex());
+			bubbleDown(0, rootElement());
+		}
+		else {
+			storage.remove(0);
+		}
+		return root;
 	}
+
+	private void bubbleDown(int rInx, Integer root) {
+		int childInx = extractBubbleDownCandidateInx(rInx, root);
+		if (childInx != -1 && heapPropertyViolated(storage.get(childInx), root)) {
+			swap(childInx, rInx);
+			bubbleDown(childInx, root);
+		}
+
+	}
+
+	private boolean childExist(int inx) {
+		return inx < storage.size();
+	}
+
+	private int lastIndex() {
+		return storage.size() - 1;
+	}
+
+	private Integer rootElement() {
+		if (storage.isEmpty())
+			throw new UnsupportedOperationException("Root element cannot be extracted on empty heap");
+		return storage.get(0);
+	}
+
 	public Integer size() {
 		return storage.size();
 	}
-	public void insert(Integer value) {
-		storage.add(value);
-		
+
+	public void insert(Integer newValue) {
+		storage.add(newValue);
+		if (heapHasMoreThenOneElement()) {
+			int vInx = lastIndex();
+			int pInx = parentIndex(vInx);
+			Integer parentValue = storage.get(pInx);
+
+			bubbleUp(vInx, newValue, pInx, parentValue);
+		}
+	}
+
+	private void bubbleUp(int vInx, Integer newValue, int pInx, Integer parentValue) {
+		if (heapPropertyViolated(newValue, parentValue)) {
+			swap(vInx, pInx);
+			if (!isRoot(pInx)) {
+				int renuedVInx = pInx;
+				int renuedPInx = parentIndex(renuedVInx);
+				Integer renuedParentValue = storage.get(renuedPInx);
+				bubbleUp(renuedVInx, newValue, renuedPInx, renuedParentValue);
+			}
+		}
+	}
+
+	private boolean isRoot(int pInx) {
+		return pInx == 0;
+	}
+
+	private void swap(int vInx, int pInx) {
+		Integer parentValue = storage.get(pInx);
+		Integer value = storage.set(vInx, parentValue);
+		storage.set(pInx, value);
+	}
+
+	private boolean heapPropertyViolated(Integer newValue, Integer parentValue) {
+
+		return newValue.compareTo(parentValue) < 0;
+	}
+
+	private boolean heapHasMoreThenOneElement() {
+		return storage.size() > 1;
+	}
+
+	public int extractBubbleDownCandidateInx(int rootInx, Integer root) {
+
+		int firstChildIndex = firstChildIndex(rootInx);
+		int secondChildIndex = secondChildIndex(rootInx);
+		int[] children = new int[2];
+		if (childExist(firstChildIndex)) {
+			children[0] = firstChildIndex;
+		}
+		else {
+			// bad practice
+			return -1;
+		}
+
+		if (childExist(secondChildIndex)) {
+			children[1] = secondChildIndex;
+		}
+		else {
+			return children[0];
+		}
+
+		return storage.get(children[0]) > storage.get(children[1]) ? children[1] : children[0];
+	}
+
+	public void heapoize(Integer[] unsortedArray) {
+		for (Integer integer : unsortedArray) {
+			insert(integer);
+		}		
+	}
+
+	public List<Integer> drain() {
+		List<Integer> result = new ArrayList<>(size());
+		while (size() > 0) {
+			result.add(extract());
+		}
+		return result;
 	}
 
 }
-
--- org.sdo.alg/src/test/java/org/sdo/algorythms/heap/HeapTest.java
+++ org.sdo.alg/src/test/java/org/sdo/algorythms/heap/HeapTest.java
@@ -1,17 +1,15 @@
 package org.sdo.algorythms.heap;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
 public class HeapTest {
-	@Test
-	public void verify_that_when_one_element_is_removed_then_tree_is_reordered() {
-		Heap heap = new Heap(new Integer[] { 4, 4, 8, 9, 4, 12, 9, 11, 13 });
-		Integer poppedVal = heap.extract();
-		assertThat(heap.extract(), is(4));
-	}
 
 	@Test
 	public void verify_that_bigger_key_doesnt_affect_root_element() {
@@ -21,10 +19,48 @@ public class HeapTest {
 	}
 
 	@Test
-	public void verify_that_smaller_key_becomes_new_root_element() {
+	public void verify_that_smallest_key_becomes_new_root_element() {
 		Heap heap = new Heap(new Integer[] { 4, 4, 8, 9 });
 		heap.insert(1);
+		assertThat(heap.size(), is(5));
 		assertThat(heap.extract(), is(1));
+	}
+
+	@Test
+	public void verify_that_heapoaze_unsorted_array_make_it_sorted() {
+		// given
+		Integer[] unsortedArray = new Integer[] { 5, 8, 13, 5, 21, 6, 3, 7, -2, 4, 8, 12 };
+		// when heapoise it
+		Heap heap = new Heap();
+		heap.heapoize(unsortedArray);
+		
+		// then exhausting heap will return us sorted sequence
+		List<Integer> result = new ArrayList<>(unsortedArray.length);
+
+		result = heap.drain();
+				
+		assertThat(result, is(Arrays.asList(-2, 3, 4, 5, 5, 6, 7, 8, 8, 12, 13, 21)));
+	}
+
+	@Test
+	public void verify_that_root_will_use_5_node_for_bubble_down() {
+		Heap heap = new Heap(new Integer[] { 4, 7, 5, 8 });
+		int candidate = heap.extractBubbleDownCandidateInx(0, 4);
+		assertThat(candidate, is(2));
+	}
+
+	@Test
+	public void verify_that_root_will_use_single_child() {
+		Heap heap = new Heap(new Integer[] { 4, 7, 5, 8 });
+		int candidate = heap.extractBubbleDownCandidateInx(1, 7);
+		assertThat(candidate, is(3));
+	}
+
+	@Test
+	public void verify_that_no_children_returned_if_they_are_not_exist() {
+		Heap heap = new Heap(new Integer[] { 4, 7, 5, 8 });
+		int candidate = heap.extractBubbleDownCandidateInx(2, 5);
+		assertThat(candidate, is(-1));
 	}
 
 	@Test
@@ -33,19 +69,11 @@ public class HeapTest {
 		assertThat(heap.extract(), is(4));
 		assertThat(heap.size(), is(8));
 	}
-
-	@Test
-	public void verify_that_we_can_store_heap_in_array() {
-		// given a heap
-		Heap heap = new Heap(new Integer[] { 4, 4, 8, 9, 4, 12, 9, 11, 13 });
-		assertThat(heap.parent(3), is(4));
-		assertThat(heap.parent(2), is(4));
-
-		assertThat(heap.children(2)[0], is(12));
-		assertThat(heap.children(2)[1], is(9));
-
-		assertThat(heap.children(3)[0], is(11));
-		assertThat(heap.children(3)[1], is(13));
+	
+	@Test(expected=UnsupportedOperationException.class)
+	public void verify_that_extract_cannot_be_performed_on_empty_heap() {
+		Heap heap = new Heap();
+		heap.extract();		
 	}
 
 }
