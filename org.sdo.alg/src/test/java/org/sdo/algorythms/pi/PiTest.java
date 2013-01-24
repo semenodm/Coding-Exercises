96d97f26896c10ead739786ac01a892227bf6e11
dsemenov <sdo.semenov@gmail.com>
2013-01-24 20:45:30 +0200

    Pi calculation

--- org.sdo.alg/src/test/java/org/sdo/algorythms/pi/PiTest.java
+++ org.sdo.alg/src/test/java/org/sdo/algorythms/pi/PiTest.java
@@ -1,8 +1,9 @@
 package org.sdo.algorythms.pi;
 
-import org.hamcrest.MatcherAssert;
 import org.junit.Test;
-import spock.util.matcher.HamcrestMatchers;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static spock.util.matcher.HamcrestMatchers.closeTo;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,17 +14,17 @@ import spock.util.matcher.HamcrestMatchers;
  */
 public class PiTest {
     @Test
-    public void verify_that_pi_with_one_square_equals_0_866() {
-        MatcherAssert.assertThat(Pi.pi(1), HamcrestMatchers.closeTo(3.4641, 0.001));
+    public void verify_that_pi_with_one_square_equals_3_4641() {
+        assertThat(Pi.pi(1), closeTo(3.4641, 0.001));
     }
 
     @Test
     public void verify_that_we_can_calc_pi_with_more_stems() {
-        MatcherAssert.assertThat(Pi.pi(2000), HamcrestMatchers.closeTo(3.1415, 0.0001));
+        assertThat(Pi.pi(2000), closeTo(3.1415, 0.0001));
     }
 
     @Test
     public void verify_that_i_can_calculate_pi_with_precision_0_000001() {
-        MatcherAssert.assertThat(Pi.pi(0.001), HamcrestMatchers.closeTo(3.1415, 0.001));
+        assertThat(Pi.pi(0.001), closeTo(3.1415, 0.001));
     }
 }
