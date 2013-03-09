f08cd4b87bd3ecae90a9f3bc6bd939bc9fad7aca
dsemenov <sdo.semenov@gmail.com>
2013-03-09 18:02:53 +0200

    add assertion into test

--- camel/src/test/java/org/sdo/camel/CamelSplitAggregateTest.java
+++ camel/src/test/java/org/sdo/camel/CamelSplitAggregateTest.java
@@ -1,12 +1,15 @@
 package org.sdo.camel;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.Endpoint;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
-import org.apache.camel.spi.RouteContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -23,19 +26,32 @@ import java.util.List;
 @ContextConfiguration(locations = "/split-aggregation.ctx.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
 public class CamelSplitAggregateTest {
-//    @Autowired
-//    RouteContext context;
+    @Autowired
+    CamelContext context;
 
     @Produce(uri = "direct:input")
     protected ProducerTemplate template;
 
+    @Before
+    public void setup() throws Exception {
+        context.getRouteDefinition("test_route").adviceWith(context, new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                interceptSendToEndpoint("file:./").skipSendToOriginalEndpoint().to("mock:advised");
+            }
+        });
+    }
+
     @Test
-    public void checkSplitAggregateRoute() {
+    public void checkSplitAggregateRoute() throws InterruptedException {
+        MockEndpoint mock = (MockEndpoint) context.getRouteDefinition("test_route").resolveEndpoint(context, "mock:advised");
+        mock.expectedBodiesReceived("Dima,32.5\nKristina,16.0\nSonya,6.0\nLiza,3.5");
         List<MyBean> input = new ArrayList<MyBean>();
         input.add(new MyBean("Dima", 32.5));
-        input.add(new MyBean("Kristina", 29.0));
+        input.add(new MyBean("Kristina", 16.0));
         input.add(new MyBean("Sonya", 6.0));
         input.add(new MyBean("Liza", 3.5));
         template.sendBody(input);
+        mock.assertIsSatisfied();
     }
 }
