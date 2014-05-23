00758f4ff07ec134077ee8f60bc0a2c8ffbe5e6d
dsemenov <sdo.semenov@gmail.com>
2014-05-23 16:13:26 -0400

    Euler 25 solution Optimization 1. get rid of collatz streams and calculate the length on the fly

--- org.sdo.alg/src/main/scala/org/sdo/alg/euler/Euler14.scala
+++ org.sdo.alg/src/main/scala/org/sdo/alg/euler/Euler14.scala
@@ -17,20 +17,20 @@ object Euler14 {
     def unapply(x:Long) = if(x==1) Some(x) else None
   }
 
-  def collatzSeq(sead : Long) : Stream[Long] = {
+  def collatzSeq(sead : Long) : Long = {
      sead match {
-       case 1 => Stream()
-       case Odd(x) => (3 * sead + 1) #:: collatzSeq(3 * sead + 1)
-       case Even(x) => (sead/2) #:: collatzSeq(sead/2)
+       case 1 => 1
+       case Odd(x) => 1 + collatzSeq(3 * sead + 1)
+       case Even(x) => 1 + collatzSeq(sead/2)
      }
   }
 
   def solution(n : Long) : Stream[Long] = {
-    (n #:: collatzSeq(n)).takeWhile(_ > 0).size #:: solution(n+1)
+    collatzSeq(n) #:: solution(n+1)
   }
 
   def main(args: Array[String]) {
-    println(collatzSeq(13).takeWhile(_ > 0).toList)
+    println(collatzSeq(13))
 
     println(solution(1).take(1000000).zipWithIndex.maxBy(_._1)._2 + 1)
   }
